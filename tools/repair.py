298506fa6bf7521186de2927f162fe11e4970bd3
David Cournapeau <cournape@gmail.com>
2014-03-31 18:01:32 +0100

    FEAT: tool to repair broken egg-info directories.
    This should be used for eggs installed with enstaller 4.6.3. The script is
    idempotent, and should only be used on windows.

---
+++ tools/repair.py
@@ -0,0 +1,28 @@
+import argparse
+import os.path
+import sys
+
+from egginst._repair_broken_egg_info import repair
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    p = argparse.ArgumentParser(description="Script to repair egg info directories.")
+    p.add_argument("-n", "--dry-run", help="Do not modity anything", action="store_true")
+    p.add_argument("--prefix", help="The prefix to fix", default=sys.prefix)
+    ns = p.parse_args(argv)
+
+    prefix = ns.prefix
+    if not os.path.exists(prefix):
+        p.error("Prefix {0} does not exist".format(prefix))
+
+    if not sys.platform == "win32":
+        p.error("This script should only be used on windows.")
+
+    repair(prefix, ns.dry_run)
+
+
+if __name__ == "__main__":
+    main()
