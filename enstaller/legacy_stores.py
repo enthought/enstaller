08f3f50eaee87594ead54c81710453821713a78f
David Cournapeau <cournape@gmail.com>
2014-05-01 18:17:31 +0100

    FEAT: add new functions to parse indices without stores.

---
+++ enstaller/legacy_stores.py
@@ -0,0 +1,94 @@
+import contextlib
+import json
+import urllib2
+import urlparse
+
+from enstaller.errors import InvalidConfiguration
+from enstaller.repository import RepositoryPackageMetadata
+from enstaller.store.cached import CachedHandler
+from enstaller.store.compressed import CompressedHandler
+from enstaller.store.indexed import _INDEX_NAME
+from enstaller.utils import PY_VER
+
+
+class URLFetcher(object):
+    def __init__(self, cache_dir, auth=None):
+        self._auth = auth
+        self.cache_dir= cache_dir
+
+    @property
+    def _opener(self):
+        """ Create custom urlopener with Compression and Caching handlers. """
+        # Use handlers from urllib2's default opener, since we already
+        # added our proxy handler to it.
+        opener = urllib2._opener
+        http_handlers = [urllib2.HTTPHandler, urllib2.HTTPSHandler]
+        handlers = opener.handlers if opener is not None else http_handlers
+
+        # Add our handlers to the default handlers.
+        handlers_ = [CompressedHandler, CachedHandler(self.cache_dir)] + handlers
+
+        return urllib2.build_opener(*handlers_)
+
+    def open(self, url):
+        request = urllib2.Request(url)
+        if self._auth is not None:
+            encoded_auth = "{0}:{1}".format(*self._auth).encode("base64").strip()
+            request.add_unredirected_header("Authorization", "Basic " +
+                                            encoded_auth)
+        request.add_header('User-Agent', 'enstaller')
+        return contextlib.closing(self._opener.open(request))
+
+
+def _parse_index(json_dict, store_location):
+    for key, info in json_dict.items():
+        info.setdefault('type', 'egg')
+        info.setdefault('python', PY_VER)
+        info.setdefault('packages', [])
+        info["store_location"] = store_location
+        yield RepositoryPackageMetadata.from_json_dict(key, info)
+
+
+def _webservice_index_parser(config):
+    url = config.webservice_entry_point + _INDEX_NAME
+    if config.use_pypi:
+        url +=  "?pypi=true"
+    else:
+        url +=  "?pypi=false"
+
+    store_location = config.webservice_entry_point
+
+    fetcher = URLFetcher(config.local, config.get_auth())
+    with fetcher.open(url) as fp:
+        json_dict = json.load(fp)
+
+    return _parse_index(json_dict, store_location)
+
+
+def _old_legacy_index_parser(config):
+    for url in config.IndexedRepos:
+        index = url +  _INDEX_NAME
+        p = urlparse.urlparse(index)
+        scheme = p.scheme
+        if scheme in ("http", "https"):
+            fetcher = URLFetcher(config.local, config.get_auth())
+            with fetcher.open(index) as fp:
+                json_dict = json.load(fp)
+        elif scheme in ("file",):
+            with open(p.path, "rb") as fp:
+                json_dict = json.load(fp)
+        else:
+            raise InvalidConfiguration("Unsupported uri: {0!r}".format(url))
+
+        for package in _parse_index(json_dict, url):
+            yield package
+
+
+def legacy_index_parser(config):
+    """
+    Yield RepositoryPackageMetadata instances from the configured stores.
+    """
+    if config.use_webservice:
+        return _webservice_index_parser(config)
+    else:
+        return _old_legacy_index_parser(config)
---
+++ enstaller/tests/test_legacy_stores.py
@@ -0,0 +1,128 @@
+import contextlib
+import json
+import os.path
+import shutil
+import sys
+import tempfile
+
+from cStringIO import StringIO
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+import mock
+
+from egginst.testing_utils import network
+from egginst.tests.common import mkdtemp
+
+from enstaller.config import Configuration
+from enstaller.legacy_stores import URLFetcher
+from enstaller.legacy_stores import _old_legacy_index_parser, _webservice_index_parser
+
+from enstaller.tests.common import dummy_repository_package_factory
+
+
+def package_metadata_iterator(store_location):
+    entries = [
+        dummy_repository_package_factory("numpy", "1.8.0", 1,
+                                         store_location=store_location),
+        dummy_repository_package_factory("scipy", "0.14.0", 1,
+                                         store_location=store_location)
+    ]
+    data = StringIO(json.dumps(dict((entry.key, entry.s3index_data) for entry in entries)))
+    return contextlib.closing(data)
+
+
+class TestLegacyStores(unittest.TestCase):
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+    @network
+    def test_url_fetcher(self):
+        # Given
+        url = "https://api.enthought.com/accounts/user/info"
+        r_user_info = {
+            'first_name': None,
+            'has_subscription': False,
+            'is_active': False,
+            'is_authenticated': False,
+            'last_name': None,
+            'subscription_level': u'unregistered'
+        }
+
+        # When
+        with mkdtemp() as tempdir:
+            fetcher = URLFetcher(tempdir)
+            with fetcher.open(url) as fp:
+                json_data = json.load(fp)
+
+        # Then
+        self.assertEqual(json_data, r_user_info)
+
+    def test_simple_webservice(self):
+        # Given
+        config = Configuration()
+        config.use_webservice = True
+        config.use_pypi = False
+
+        # When
+        with mock.patch("enstaller.legacy_stores.URLFetcher.open",
+                        return_value=package_metadata_iterator("")):
+            packages = list(_webservice_index_parser(config))
+
+        # Then
+        self.assertTrue(len(packages) > 0)
+
+    def test_simple_no_webservice_https(self):
+        # Given
+        config = Configuration()
+        config.IndexedRepos = [
+            'https://www.enthought.com/repo/epd/eggs/{SUBDIR}/',
+        ]
+        config.use_webservice = False
+
+        # When
+        with mock.patch("enstaller.legacy_stores.URLFetcher.open",
+                        return_value=package_metadata_iterator("")):
+            packages = list(_old_legacy_index_parser(config))
+
+        # Then
+        self.assertTrue(len(packages) > 0)
+
+    def test_simple_no_webservice_file(self):
+        # Given
+        fake_index = {
+           "zope.testing-3.8.3-1.egg": {
+               "available": True,
+               "build": 1,
+               "md5": "6041fd75b7fe9187ccef0d40332c6c16",
+               "mtime": 1262725254.0,
+               "name": "zope.testing",
+               "product": "commercial",
+               "python": "2.6",
+               "size": 514439,
+               "type": "egg",
+               "version": "3.8.3",
+           }
+        }
+        index_path = os.path.join(self.tempdir, "index.json")
+        with open(index_path, "w") as fp:
+            fp.write(json.dumps(fake_index))
+
+        config = Configuration()
+        config.IndexedRepos = [
+            "file://{0}".format(self.tempdir)
+        ]
+        config.use_webservice = False
+
+        # When
+        packages = list(_old_legacy_index_parser(config))
+
+        # Then
+        self.assertEqual(len(packages), 1)
+        self.assertEqual(packages[0].key, "zope.testing-3.8.3-1.egg")
