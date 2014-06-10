704a19876f499521a91cf7d51622a03c79036ad6
David Cournapeau <cournape@gmail.com>
2014-06-10 15:21:01 +0100

    FEAT: add simple class to simplify iterating over a response.

--- enstaller/requests_utils.py
+++ enstaller/requests_utils.py
@@ -59,3 +59,32 @@ class LocalFileAdapter(requests.adapters.HTTPAdapter):
              verify=True, cert=None, proxies=None):
 
         return self.build_response_from_file(request, stream)
+
+
+class _ResponseIterator(object):
+    """
+    A simple iterator on top of a requests response
+
+    It supports the `len` protocol so that packages such as click can show an
+    ETA when fetching by chunk.
+
+    Example
+    -------
+    >>> resp = requests.get("http://acme.com", stream=True)
+    >>> for chunk in _ResponseIterator(resp):
+        print len(chunk)
+    """
+    def __init__(self, response):
+        self._response = response
+        self._size = int(self._response.headers.get("content-length", 0))
+        self._chunk_size = 1024
+
+    def __iter__(self):
+        self._iter = self._response.iter_content(self._chunk_size)
+        return self
+
+    def next(self):
+        return self._iter.next()
+
+    def __len__(self):
+        return int(self._size / self._chunk_size + 1)
--- enstaller/tests/test_request_utils.py
+++ enstaller/tests/test_request_utils.py
@@ -1,5 +1,7 @@
 import os.path
+import shutil
 import sys
+import tempfile
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
@@ -8,7 +10,9 @@ else:
 
 import requests
 
+from enstaller.requests_utils import _ResponseIterator
 from enstaller.requests_utils import FileResponse, LocalFileAdapter
+from enstaller.utils import compute_md5
 
 
 class TestFileResponse(unittest.TestCase):
@@ -67,3 +71,48 @@ class TestLocalFileAdapter(unittest.TestCase):
         # Then
         self.assertEqual(data, r_data)
         self.assertEqual(resp.headers["content-length"], str(os.stat(__file__).st_size))
+
+
+class TestResponseIterator(unittest.TestCase):
+    def setUp(self):
+        self.prefix = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.prefix)
+
+    def _create_file_session(self):
+        session = requests.session()
+        session.mount("file://", LocalFileAdapter())
+
+        return session
+
+    def test_simple(self):
+        # Given
+        session = self._create_file_session()
+        source = __file__
+        target = os.path.join(self.prefix, "output.dat")
+
+        # When
+        resp = session.get("file://{0}".format(os.path.abspath(source)))
+        with open(target, "w") as fp:
+            for chunk in _ResponseIterator(resp):
+                fp.write(chunk)
+
+        # Then
+        self.assertEqual(compute_md5(target), compute_md5(source))
+
+    def test_without_content_length(self):
+        # Given
+        session = self._create_file_session()
+        source = __file__
+        target = os.path.join(self.prefix, "output.dat")
+
+        # When
+        resp = session.get("file://{0}".format(os.path.abspath(source)))
+        resp.headers.pop("content-length")
+        with open(target, "w") as fp:
+            for chunk in _ResponseIterator(resp):
+                fp.write(chunk)
+
+        # Then
+        self.assertEqual(compute_md5(target), compute_md5(source))
