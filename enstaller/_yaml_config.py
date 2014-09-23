6c6955da28c1f267c975c84b3d713de343e4043f
David Cournapeau <cournape@gmail.com>
2014-09-23 17:27:05 +0100

    FEAT: add a new yaml-based format for enstaller.
    This new format assumes enpkg will talk to a brood server.

---
+++ enstaller/_yaml_config.py
@@ -0,0 +1,116 @@
+import os.path
+
+from egginst._compat import string_types
+
+from enstaller.errors import InvalidConfiguration
+from enstaller.plat import custom_plat
+from enstaller.utils import fill_url
+from enstaller.vendor import jsonschema, yaml
+
+
+_AUTHENTICATION = "authentication"
+_AUTHENTICATION_TYPE = "type"
+_AUTHENTICATION_TYPE_BASIC = "basic"
+_AUTHENTICATION_TYPE_DIGEST = "digest"
+_USERNAME = "username"
+_PASSWORD = "password"
+_REPOSITORIES = "repositories"
+_FILES_CACHE = "files_cache"
+_STORE_URL = "store_url"
+
+_SCHEMA = {
+   "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "EnstallerConfiguration",
+    "description": "Enstaller >= 4.8.0 configuration",
+    "type": "object",
+    "properties": {
+        "store_url": {
+            "description": "The url (schema + hostname only of the store to connect to).",
+            "type": "string"
+        },
+        "files_cache": {
+            "description": "Where to cache downloaded files.",
+            "type": "string"
+        },
+        "authentication": {
+            "type": "object",
+            "oneOf": [
+                {"$ref": "#/definitions/simple_authentication"},
+                {"$ref": "#/definitions/digest_authentication"}
+            ],
+            "description": "Authentication."
+        },
+        "repositories": {
+            "type": "array",
+            "items": {"type": "string"},
+            "description": "List of repositories."
+        }
+    },
+    "definitions": {
+        "simple_authentication": {
+            "properties": {
+                "type": { "enum": [ "basic" ], "default": "basic" },
+                "username": {"type": "string" },
+                "password": {"type": "string"}
+            },
+            "required": [ "username", "password" ],
+            "additionalProperties": False
+        },
+        "digest_authentication": {
+            "properties": {
+                "type": { "enum": [ "digest" ]},
+                "auth": {"type": "string" }
+            },
+            "required": [ "type", "auth" ],
+            "additionalProperties": False
+        }
+    },
+    "additionalProperties": False,
+}
+
+def load_configuration_from_yaml(cls, filename_or_fp):
+    # FIXME: local import to workaround circular import
+    from enstaller.config import _decode_auth
+    if isinstance(filename_or_fp, string_types):
+        with open(filename_or_fp, "rt") as fp:
+            data = yaml.load(fp)
+    else:
+        data = yaml.load(filename_or_fp)
+
+    if data is None:
+        data = {}
+    else:
+        jsonschema.validate(data, _SCHEMA)
+
+    config = cls()
+
+    if _AUTHENTICATION in data:
+        authentication = data[_AUTHENTICATION]
+        authentication_type = authentication.get(_AUTHENTICATION_TYPE,
+                                                 _AUTHENTICATION_TYPE_BASIC)
+        if authentication_type == _AUTHENTICATION_TYPE_BASIC:
+            username = authentication[_USERNAME]
+            password = authentication[_PASSWORD]
+        elif authentication_type == _AUTHENTICATION_TYPE_DIGEST:
+            username, password = _decode_auth(authentication["auth"])
+        else:
+            msg = "Unknown authentication type {0!r}".format(authentication_type)
+            raise InvalidConfiguration(msg)
+        config.set_auth(username, password)
+
+    if _STORE_URL in data:
+        config.set_store_url(data[_STORE_URL])
+    if _REPOSITORIES in data:
+        repository_urls = [config.store_url + "/repo/{}/{{PLATFORM}}".format(repository)
+                           for repository in data[_REPOSITORIES]]
+        config.set_indexed_repositories(repository_urls)
+    if _FILES_CACHE in data:
+        files_cache = os.path.expanduser(data[_FILES_CACHE]). \
+                         replace("{PLATFORM}", custom_plat)
+        config._repository_cache = files_cache
+ 
+    config.disable_webservice()
+
+    if isinstance(filename_or_fp, string_types):
+        config._filename = filename_or_fp
+    return config
