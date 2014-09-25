9c65d039bb11065339d57a8d07e0da45c3981848
David Cournapeau <cournape@gmail.com>
2014-09-25 10:02:07 +0100

    MAINT: we can now use a released okonomiyaki.

--- enstaller/tools/repack.py
+++ enstaller/tools/repack.py
@@ -1,4 +1,7 @@
+import argparse
+import os
 import os.path
+import sys
 
 from okonomiyaki.errors import OkonomiyakiError
 from okonomiyaki.file_formats.egg import (_SPEC_DEPEND_LOCATION,
@@ -92,3 +95,26 @@ def repack(source_egg_path, build_number=1, platform_string=None):
                 target.writestr(source.getinfo(archive), source.read(archive))
 
             target.writestr(_SPEC_DEPEND_LOCATION, legacy_spec.to_string())
+
+
+def main(argv=None):
+    argv = argv or sys.argv[1:]
+
+    p = argparse.ArgumentParser("Egg repacker.")
+    p.add_argument("egg", help="Path to the egg to repack.")
+    p.add_argument("-b", "--build", dest="build_number",
+                   help="Build number (default is %(default)s)",
+                   default=1, type=int)
+    p.add_argument("-a", dest="platform_string",
+                   help="Legacy epd platform string (e.g. 'rh5-32'). "
+                        "Will be guessed if not specified.")
+    ns = p.parse_args(argv)
+
+    if os.path.exists(ENDIST_DAT):
+        p.error("{0!r} files are not handled yet.".format(ENDIST_DAT))
+
+    repack(ns.egg, ns.build_number, ns.platform_string)
+
+
+if __name__ == "__main__":
+    main()
