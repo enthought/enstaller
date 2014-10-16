9fc38fc6e9f70d5f2ce052cb9a882d0ea4cd30c0
David Cournapeau <cournape@gmail.com>
2014-10-16 15:54:12 +0100

    BUG: fix warning message.

--- enstaller/config.py
+++ enstaller/config.py
@@ -267,8 +267,9 @@ def _create_error_message(fp, exc):
 class Configuration(object):
     @classmethod
     def _from_legacy_locations(cls):
-        warnings.warn("get_path deprecated, use Configuration.from_filename "
-                    "with an explicit filename", DeprecationWarning)
+        warnings.warn("Using legacy location is deprecated, use "
+                      "Configuration.from_filename with an explicit "
+                      "filename instead", DeprecationWarning)
         config_path = None
         for p in configuration_read_search_order():
             candidate = os.path.join(p, ENSTALLER4RC_FILENAME)
