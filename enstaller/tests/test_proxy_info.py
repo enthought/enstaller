939fc9d6287cdb920032c4e40d08426ececc2351
David Cournapeau <cournape@gmail.com>
2014-11-05 17:03:07 +0000

    MAINT: skip broken 2.6 test because of stdlib bug.

--- enstaller/tests/test_proxy_info.py
+++ enstaller/tests/test_proxy_info.py
@@ -1,9 +1,6 @@
 import sys
 
-if sys.version_info < (2, 7):
-    import unittest2 as unittest
-else:
-    import unittest
+from egginst.vendor.six.moves import unittest
 
 from enstaller.errors import InvalidConfiguration
 from enstaller.proxy_info import ProxyInfo
@@ -25,6 +22,8 @@ class TestProxyInfoFromString(unittest.TestCase):
         with self.assertRaises(InvalidConfiguration):
             ProxyInfo.from_string(s)
 
+    @unittest.skipIf(sys.version_info < (2, 7),
+                     "Bug in stdlib on 2.6")
     def test_without_scheme(self):
         # Given
         s = "acme.com:3129"
