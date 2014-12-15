00e502f7186de6d2ee180626341773023badce5b
David Cournapeau <cournape@gmail.com>
2014-12-15 19:06:19 +0000

    FEAT: add IRepositoryInfo classes.

---
+++ enstaller/repository_info.py
@@ -0,0 +1,82 @@
+import abc
+import posixpath
+
+import enstaller.plat
+
+from egginst._compat import with_metaclass
+from egginst.vendor.six.moves import urllib
+
+from enstaller.auth import _INDEX_NAME
+
+
+class IRepositoryInfo(with_metaclass(abc.ABCMeta)):
+    @abc.abstractproperty
+    def index_url(self):
+        """ The exact url to fetch the index at."""
+
+    @abc.abstractproperty
+    def name(self):
+        """ An arbitrary string identifying the repository."""
+
+    @abc.abstractmethod
+    def _package_url(self, package):
+        """ The exact url to fetch the given package at.
+
+        Parameters
+        ----------
+        package : PackageMetadata
+            the package metadata
+        """
+
+
+class LegacyRepositoryInfo(IRepositoryInfo):
+    def __init__(self, store_url):
+        self._store_url = store_url
+
+    @property
+    def index_url(self):
+        return urllib.parse.urljoin(self._store_url, _INDEX_NAME)
+
+    @property
+    def name(self):
+        return self._store_url
+
+    def _package_url(self, package):
+        return urllib.parse.urljoin(self._store_url, package.key)
+
+
+class BroodRepositoryInfo(IRepositoryInfo):
+    def __init__(self, store_url, name, platform=None):
+        self._platform = platform or enstaller.plat.custom_plat
+        self._name = name
+        self._store_url = store_url
+
+        self._path = "/repo/{0._name}/{0._platform}".format(self)
+
+    @property
+    def index_url(self):
+        return urllib.parse.urljoin(self._store_url, self._path + "/" + _INDEX_NAME)
+
+    @property
+    def name(self):
+        return self._name
+
+    def _package_url(self, package):
+        return urllib.parse.urljoin(self._store_url, self._path + "/" + package.key)
+
+
+class FSRepositoryInfo(IRepositoryInfo):
+    def __init__(self, store_url, name):
+        self._name = name
+        self._store_url = store_url
+
+    @property
+    def index_url(self):
+        return posixpath.join(self._store_url, _INDEX_NAME)
+
+    @property
+    def name(self):
+        return self._name
+
+    def _package_url(self, package):
+        return posixpath.join(self._store_url, package.key)
---
+++ enstaller/tests/test_repository_info.py
@@ -0,0 +1,73 @@
+import os.path
+import posixpath
+
+import enstaller.plat
+
+from egginst.tests.common import DUMMY_EGG
+from egginst.vendor.six.moves import unittest
+
+from enstaller.repository_info import BroodRepositoryInfo, FSRepositoryInfo, LegacyRepositoryInfo
+from enstaller.repository import RepositoryPackageMetadata
+from enstaller.utils import path_to_uri
+
+
+class TestLegacyRepositoryInfo(unittest.TestCase):
+    def test_simple(self):
+        # Given
+        store_url = "https://acme.com"
+        path = DUMMY_EGG
+
+        r_index_url = "https://acme.com/index.json"
+        r_package_url = "https://acme.com/{0}".format(os.path.basename(path))
+
+        # When
+        info = LegacyRepositoryInfo(store_url)
+        package = RepositoryPackageMetadata.from_egg(path)
+
+        # Then
+        self.assertEqual(info.index_url, r_index_url)
+        self.assertEqual(info.name, store_url)
+        self.assertEqual(info._package_url(package), r_package_url)
+
+
+class TestLegacyBroodRepositoryInfo(unittest.TestCase):
+    def test_simple(self):
+        # Given
+        store_url = "https://acme.com"
+        name = "enthought/free"
+        path = DUMMY_EGG
+
+        r_index_url = ("https://acme.com/repo/{0}/{1}/index.json"
+                       .format(name, enstaller.plat.custom_plat))
+        r_package_url = ("https://acme.com/repo/{0}/{1}/{2}".
+                         format(name, enstaller.plat.custom_plat,
+                                os.path.basename(path)))
+
+        # When
+        info = BroodRepositoryInfo(store_url, name)
+        package = RepositoryPackageMetadata.from_egg(path)
+
+        # Then
+        self.assertEqual(info.index_url, r_index_url)
+        self.assertEqual(info.name, name)
+        self.assertEqual(info._package_url(package), r_package_url)
+
+
+class TestFSRepositoryInfo(unittest.TestCase):
+    def test_simple(self):
+        # Given
+        path = DUMMY_EGG
+        store_url = path_to_uri(os.path.dirname(path))
+        name = "enthought/free"
+
+        r_index_url = posixpath.join(store_url, "index.json")
+        r_package_url = path_to_uri(path)
+
+        # When
+        info = FSRepositoryInfo(store_url, name)
+        package = RepositoryPackageMetadata.from_egg(path)
+
+        # Then
+        self.assertEqual(info.index_url, r_index_url)
+        self.assertEqual(info.name, name)
+        self.assertEqual(info._package_url(package), r_package_url)
