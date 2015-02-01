37aa40b0d8a92f4fde264b29cf3aca0ba4da179f
David Cournapeau <cournape@gmail.com>
2015-02-01 23:18:32 +0000

    BUG: fix version normalization.

--- enstaller/new_solver/_composer_utils.py
+++ enstaller/new_solver/_composer_utils.py
@@ -5,16 +5,36 @@ don't use this in enstaller itself.
 import collections
 import json
 
+from enstaller.repository_info import BroodRepositoryInfo
+
 from enstaller.new_solver.constraint_types import Any, EnpkgUpstreamMatch, Equal
 from enstaller.new_solver.requirement import Requirement
+from enstaller.new_solver.yaml_utils import repository_factory
 
+
+def fix_php_version(version):
+    """ 'Normalize' an EnpkgVersion to a valid composer version
+    string.
+    """
+    upstream = str(version.upstream)
+    while upstream.count(".") < 2:
+        upstream += ".0"
+    return upstream
+
+
+_TO_NORMALIZE = {
+    "1.0a3": "1.0.0.0-alpha3",
+    "2011n": "2011.0.0.0",
+    "0.14.1rc1": "0.14.1.0-RC1",
+}
 
 def normalized_php_version(version):
     """ 'Normalize' an EnpkgVersion to a valid normalized composer version
     string.
     """
     upstream = str(version.upstream)
-    if upstream.count(".") < 3:
+    upstream = _TO_NORMALIZE.get(upstream, upstream)
+    while upstream.count(".") < 3:
         upstream += ".0"
     return upstream
 
@@ -31,7 +51,7 @@ def requirement_to_php_string(requirement):
             parts.append("*")
         elif isinstance(constraint, Equal):
             normalized = normalized_php_version(constraint.version)
-            parts.append("=={0}".format(normalized))
+            parts.append("{0}".format(normalized))
         else:
             print(type(constraint))
             raise NotImplementedError(constraint)
@@ -57,16 +77,22 @@ def repository_to_composer_json(repository):
                     p in package.dependencies]
         res.append({
             "name": package.name,
-            "version": str(package.version.upstream),
+            "version": fix_php_version(package.version),
             "version_normalized": version_normalized,
             "require": requirements_to_php_dict(requires),
         })
     return json.dumps(res, indent=4)
 
 
-if __name__ == "__main__":
-    from enstaller.new_solver.tests.common import repository_from_index
+def write_composer_repository(data, attribute_name, packages):
+    repository_info = BroodRepositoryInfo("http://acme.come", attribute_name)
+    if attribute_name == "remote":
+        remote_packages = data.get(attribute_name, packages.keys())
+    else:
+        remote_packages = data.get(attribute_name, [])
+    repository = repository_factory(remote_packages, repository_info, packages)
 
-    repository = repository_from_index("index.json")
-    with open("remote.json", "wt") as fp:
+    filename = "{}.json".format(attribute_name)
+
+    with open(filename, "wt") as fp:
         fp.write(repository_to_composer_json(repository))
