130ea47a5893df1db5d6579e712027d6d774662c
David Cournapeau <cournape@gmail.com>
2015-07-20 18:45:01 +0100

    BUG: one more attempt at fixing this path madness.

--- egginst/runtime.py
+++ egginst/runtime.py
@@ -101,11 +101,17 @@ class RuntimeInfo(object):
 
     def __init__(self, prefix, bindir, scriptsdir, site_packages, executable,
                  version_info, platform, implementation):
-        self.prefix = os.path.normpath(prefix)
-        self.bindir = os.path.normpath(bindir)
-        self.scriptsdir = os.path.normpath(scriptsdir)
-        self.site_packages = os.path.normpath(site_packages)
-        self.executable = os.path.normpath(executable)
+        def normpath(p):
+            if platform.os == WINDOWS:
+                return ntpath.normpath(p)
+            else:
+                return posixpath.normpath(p)
+
+        self.prefix = normpath(prefix)
+        self.bindir = normpath(bindir)
+        self.scriptsdir = normpath(scriptsdir)
+        self.site_packages = normpath(site_packages)
+        self.executable = normpath(executable)
 
         self.version_info = version_info
 
