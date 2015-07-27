46e7e6240b900c1981437cb8dd8b45272faab233
David Cournapeau <cournape@gmail.com>
2015-07-27 11:48:38 +0200

    MAINT: update responses to 0.4.0.
    Because of changes in responses mocking, we now need to use a custom
    activate decorator if one wants to mock file:// URLS as handled by our
    LocalFileAdapter handler.

--- enstaller/vendor/responses.py
+++ enstaller/vendor/responses.py
@@ -1,25 +1,27 @@
-"""
-Copyright 2013 Dropbox, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
 from __future__ import (
     absolute_import, print_function, division, unicode_literals
 )
 
+import inspect
 import re
 import six
+
+from collections import namedtuple, Sequence, Sized
+from functools import update_wrapper
+from cookies import Cookies
+from enstaller.vendor.requests.utils import cookiejar_from_dict
+from enstaller.vendor.requests.exceptions import ConnectionError
+from enstaller.vendor.requests.sessions import REDIRECT_STATI
+
+try:
+    from enstaller.vendor.requests.packages.urllib3.response import HTTPResponse
+except ImportError:
+    from urllib3.response import HTTPResponse
+
+if six.PY2:
+    from urlparse import urlparse, parse_qsl
+else:
+    from urllib.parse import urlparse, parse_qsl
 
 if six.PY2:
     try:
@@ -29,20 +31,53 @@ if six.PY2:
 else:
     from io import BytesIO as BufferIO
 
-from collections import namedtuple, Sequence, Sized
-from functools import wraps
-from enstaller.vendor.requests.exceptions import ConnectionError
-try:
-    from enstaller.vendor.requests.packages.urllib3.response import HTTPResponse
-except ImportError:
-    from urllib3.response import HTTPResponse
-if six.PY2:
-    from urlparse import urlparse, parse_qsl
-else:
-    from urllib.parse import urlparse, parse_qsl
-
 
 Call = namedtuple('Call', ['request', 'response'])
+
+_wrapper_template = """\
+def wrapper%(signature)s:
+    with responses:
+        return func%(funcargs)s
+"""
+
+
+def _is_string(s):
+    return isinstance(s, (six.string_types, six.text_type))
+
+
+def _is_redirect(response):
+    try:
+        # 2.0.0 <= requests <= 2.2
+        return response.is_redirect
+    except AttributeError:
+        # requests > 2.2
+        return (
+            # use request.sessions conditional
+            response.status_code in REDIRECT_STATI and
+            'location' in response.headers
+        )
+
+
+def get_wrapped(func, wrapper_template, evaldict):
+    # Preserve the argspec for the wrapped function so that testing
+    # tools such as pytest can continue to use their fixture injection.
+    args, a, kw, defaults = inspect.getargspec(func)
+
+    signature = inspect.formatargspec(args, a, kw, defaults)
+    is_bound_method = hasattr(func, '__self__')
+    if is_bound_method:
+        args = args[1:]     # Omit 'self'
+    callargs = inspect.formatargspec(args, a, kw, None)
+
+    ctx = {'signature': signature, 'funcargs': callargs}
+    six.exec_(wrapper_template % ctx, evaldict)
+
+    wrapper = evaldict['wrapper']
+
+    update_wrapper(wrapper, func)
+    if is_bound_method:
+        wrapper = wrapper.__get__(func.__self__, type(func.__self__))
+    return wrapper
 
 
 class CallList(Sequence, Sized):
@@ -65,6 +100,15 @@ class CallList(Sequence, Sized):
         self._calls = []
 
 
+def _ensure_url_default_path(url, match_querystring):
+    if _is_string(url) and url.count('/') == 2:
+        if match_querystring:
+            return url.replace('?', '/?', 1)
+        else:
+            return url + '/'
+    return url
+
+
 class RequestsMock(object):
     DELETE = 'DELETE'
     GET = 'GET'
@@ -74,9 +118,10 @@ class RequestsMock(object):
     POST = 'POST'
     PUT = 'PUT'
 
-    def __init__(self):
+    def __init__(self, assert_all_requests_are_fired=True):
         self._calls = CallList()
         self.reset()
+        self.assert_all_requests_are_fired = assert_all_requests_are_fired
 
     def reset(self):
         self._urls = []
@@ -87,9 +132,7 @@ class RequestsMock(object):
             content_type='text/plain'):
 
         # ensure the url has a default path set if the url is a string
-        if self._is_string(url) and url.count('/') == 2:
-            url = url.replace('?', '/?', 1) if match_querystring \
-                else url + '/'
+        url = _ensure_url_default_path(url, match_querystring)
 
         # body must be bytes
         if isinstance(body, six.text_type):
@@ -108,6 +151,8 @@ class RequestsMock(object):
 
     def add_callback(self, method, url, callback, match_querystring=False,
                      content_type='text/plain'):
+        # ensure the url has a default path set if the url is a string
+        # url = _ensure_url_default_path(url, match_querystring)
 
         self._urls.append({
             'url': url,
@@ -121,16 +166,17 @@ class RequestsMock(object):
     def calls(self):
         return self._calls
 
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *args):
+        self.stop()
+        self.reset()
+
     def activate(self, func):
-        @wraps(func)
-        def wrapped(*args, **kwargs):
-            self.start()
-            try:
-                return func(*args, **kwargs)
-            finally:
-                self.stop()
-                self.reset()
-        return wrapped
+        evaldict = {'responses': self, 'func': func}
+        return get_wrapped(func, _wrapper_template, evaldict)
 
     def _find_match(self, request):
         for match in self._urls:
@@ -140,14 +186,18 @@ class RequestsMock(object):
             if not self._has_url_match(match, request.url):
                 continue
 
-            return match
-
-        return None
+            break
+        else:
+            return None
+        if self.assert_all_requests_are_fired:
+            # for each found match remove the url from the stack
+            self._urls.remove(match)
+        return match
 
     def _has_url_match(self, match, request_url):
         url = match['url']
 
-        if self._is_string(url):
+        if _is_string(url):
             if match['match_querystring']:
                 return self._has_strict_url_match(url, request_url)
             else:
@@ -169,19 +219,20 @@ class RequestsMock(object):
         other_qsl = sorted(parse_qsl(other_parsed.query))
         return url_qsl == other_qsl
 
-    def _is_string(self, s):
-        return isinstance(s, (six.string_types, six.text_type))
-
-    def _on_request(self, session, request, **kwargs):
+    def _on_request(self, adapter, request, **kwargs):
         match = self._find_match(request)
-
         # TODO(dcramer): find the correct class for this
         if match is None:
-            error_msg = 'Connection refused: {0}'.format(request.url)
+            error_msg = 'Connection refused: {0} {1}'.format(request.method,
+                                                             request.url)
             response = ConnectionError(error_msg)
 
             self._calls.add(request, response)
             raise response
+
+        if 'body' in match and isinstance(match['body'], Exception):
+            self._calls.add(request, match['body'])
+            raise match['body']
 
         headers = {
             'Content-Type': match['content_type'],
@@ -207,11 +258,19 @@ class RequestsMock(object):
             preload_content=False,
         )
 
-        adapter = session.get_adapter(request.url)
-
         response = adapter.build_response(request, response)
         if not match.get('stream'):
             response.content  # NOQA
+
+        try:
+            resp_cookies = Cookies.from_request(response.headers['set-cookie'])
+            response.cookies = cookiejar_from_dict(dict(
+                (v.name, v.value)
+                for _, v
+                in resp_cookies.items()
+            ))
+        except (KeyError, TypeError):
+            pass
 
         self._calls.add(request, response)
 
@@ -220,17 +279,22 @@ class RequestsMock(object):
     def start(self):
         import mock
 
-        def unbound_on_send(session, requests, *a, **kwargs):
-            return self._on_request(session, requests, *a, **kwargs)
-        self._patcher = mock.patch('enstaller.vendor.requests.Session.send', unbound_on_send)
+        def unbound_on_send(adapter, request, *a, **kwargs):
+            return self._on_request(adapter, request, *a, **kwargs)
+        self._patcher = mock.patch('enstaller.vendor.requests.adapters.HTTPAdapter.send',
+                                   unbound_on_send)
         self._patcher.start()
 
     def stop(self):
         self._patcher.stop()
+        if self.assert_all_requests_are_fired and self._urls:
+            raise AssertionError(
+                'Not all requests has been executed {0!r}'.format(
+                    [(url['method'], url['url']) for url in self._urls]))
 
 
 # expose default mock namespace
-_default_mock = RequestsMock()
+mock = _default_mock = RequestsMock(assert_all_requests_are_fired=False)
 __all__ = []
 for __attr in (a for a in dir(_default_mock) if not a.startswith('_')):
     __all__.append(__attr)
